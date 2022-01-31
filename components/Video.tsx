762480d0b76cbb8a5905eb83bbf8670436e9162a
Cagatay Kaydir <cagataykaydir@gmail.com>
2022-01-31 03:41:16 +0300

    video component animations

--- components/Video.tsx
+++ components/Video.tsx
@@ -1,33 +1,89 @@
-import React from 'react';
+import { motion } from 'framer-motion';
+import React, { useState } from 'react';
+import useIsMobile from '../hooks/useIsMobile';
 import styles from '../styles/components/Video.module.scss';
 
 export default function Video() {
+  const isMobile = useIsMobile();
+
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [isHovering, setIsHovering] = useState(false);
+
   return (
-    <div className={styles.container}>
+    <motion.div
+      className={styles.container}
+      onHoverStart={() => setIsHovering(true)}
+      onHoverEnd={() => setIsHovering(false)}
+    >
       <div className={styles.top}>
-        <img className={styles.thumbnail} alt="thumbnail" src="https://i.ytimg.com/vi/4mSIQgQue7E/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCypn8UisozfIuEUkuMoqPEg_mK6Q" />
-        <button type="button" className={styles.play}>
-          <img src="/icons/play.svg" alt="oynat" />
-        </button>
-      </div>
+        <div className={styles.thumbnail}>
+          {
+            !isPlaying
+              ? <img alt="thumbnail" src="https://i.ytimg.com/vi/4mSIQgQue7E/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCypn8UisozfIuEUkuMoqPEg_mK6Q" />
 
-      <div className={styles.info}>
-        <div className={styles.header}>#KÇ5 KELEPİR GECKO TERARYUMU - KARGO AÇILIM VE</div>
-        {/* eslint-disable-next-line max-len */}
-        <div className={styles.description}>Kaldigimiz yerden devam! Bu bölümde transfer ettiğimiz Camponotus Sanctus kolonisini, Burak ve Fahriye için aldiğimiz yeni / geniş teraryumu ve basitçe kargo açilimi izleyecekseniz. Umarim beğenirsiniz. İyi seyirler!</div>
-      </div>
-
-      <div className={styles.bottom}>
-        <div className={styles.bottomInfo}>
-          <img src="/icons/view.svg" alt="izlenme" />
-          <div>132.000</div>
+              : <iframe width="853" height="480" src="https://www.youtube.com/embed/4mSIQgQue7E?autoplay=1" title="YouTube video player" frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen />
+          }
         </div>
 
-        <div className={styles.bottomInfo}>
-          <img src="/icons/thumb_up.svg" alt="beğeni" />
-          <div>32K</div>
-        </div>
+        {
+          !isPlaying
+          && (isMobile ? (
+            <button type="button" className={styles.play} onClick={() => setIsPlaying(true)}>
+              <img src="/icons/play.svg" alt="oynat" />
+            </button>
+          ) : (
+            <motion.button initial={{ opacity: 0, scale: 0, y: 200 }} transition={{ duration: 0.6, type: 'spring' }} animate={isHovering ? { opacity: 1, scale: 1, y: 0 } : { opacity: 0, scale: 0, y: 200 }} whileHover={{ backgroundColor: 'rgba(158, 158, 158, 0.8)' }} type="button" className={styles.play} onClick={() => setIsPlaying(true)}>
+              <img src="/icons/play.svg" alt="oynat" />
+            </motion.button>
+          ))
+        }
       </div>
-    </div>
+
+      <div style={{
+        display: 'flex', flex: 1, flexDirection: 'column', justifyContent: 'center',
+      }}
+      >
+        <div className={styles.info}>
+          <div className={styles.header}>#KÇ5 KELEPİR GECKO TERARYUMU - KARGO AÇILIM VE</div>
+          {/* eslint-disable-next-line max-len */}
+          <div className={styles.description}>Kaldigimiz yerden devam! Bu bölümde transfer ettiğimiz Camponotus Sanctus kolonisini, Burak ve Fahriye için aldiğimiz yeni / geniş teraryumu ve basitçe kargo açilimi izleyecekseniz. Umarim beğenirsiniz. İyi seyirler!</div>
+        </div>
+
+        <motion.div
+          initial={{
+            opacity: 0, scale: 0, marginTop: '-60px',
+          }}
+          transition={{ duration: 0.6, type: 'spring' }}
+          animate={isHovering ? {
+            opacity: 1, scale: 1, marginTop: '0px',
+          } : {
+            opacity: 0, scale: 0, marginTop: '-60px',
+          }}
+          className={styles.bottom}
+        >
+          <div className={styles.bottomInfo}>
+            <img src="/icons/view.svg" alt="izlenme" />
+            <div>132.000</div>
+          </div>
+
+          <div className={styles.bottomInfo}>
+            <img src="/icons/thumb_up.svg" alt="beğeni" />
+            <div>32K</div>
+          </div>
+        </motion.div>
+      </div>
+    </motion.div>
   );
 }
+
+// <div className={styles.bottom}>
+//   <div className={styles.bottomInfo}>
+//     <img src="/icons/view.svg" alt="izlenme" />
+//     <div>132.000</div>
+//   </div>
+
+//   <div className={styles.bottomInfo}>
+//     <img src="/icons/thumb_up.svg" alt="beğeni" />
+//     <div>32K</div>
+//   </div>
+// </div>
